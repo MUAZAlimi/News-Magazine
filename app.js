efe74bbceb919dd0385e2c35dd1a0fe1198e038b
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-10-23 14:47:57 +0100

    News-Magazine

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const newsRouter = require('./src/route/news');
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/js', express.static(__dirname + 'public/js'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// Templateing engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/', newsRouter)
+app.use('/article', newsRouter)
+
+app.listen(PORT, () => console.log(`News-Magazine running on port ${PORT}🎉😎🤑`));
