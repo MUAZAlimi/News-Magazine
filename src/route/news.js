031166122900112183387f1003e991fbb709fa1f
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-10-24 08:50:11 +0100

    update

--- src/route/news.js
+++ src/route/news.js
@@ -44,6 +44,27 @@ newsRouter.get('/:id', async(req, res) => {
         }
    }
 })
+newsRouter.post('', async(req, res) => {
+    let search = req.body.search
+
+   try {
+        const newsApi = await axios.get(`https://raddy.dev/wp-json/wp/v2/posts?search=${search}`);
+        res.render('newsSearch', { articles: newsApi.data})
+   } catch (error) {
+        if (error.response) {
+            res.render('newsSearch', { articles: null })
+            console.log(error.response.data);
+            console.log(error.response.headers);
+            console.log(error.response.status);
+        } else if(error.request) {
+            res.render('newsSearch', { articles: null })
+            console.log(error.response);
+        } else {
+            res.render('newsSearch', { articles: null })
+            console.log("Error", error.message);
+        }
+   }
+})
 
 
 module.exports = newsRouter
